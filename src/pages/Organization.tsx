fa43c7c1c223250822b36e549a0f7e1b1f720ff5
Marie St Mart <adriannastmart@gmail.com>
2025-01-10 12:08:17 +0100

    Update Organization.tsx

--- src/pages/Organization.tsx
+++ src/pages/Organization.tsx
@@ -7,6 +7,8 @@ const Organization: React.FC = () => {
   return (
     <div>
       <Title level={4}>Organization</Title>
+      <Divider style={{ borderColor: '#a349a4',borderWidth: '2px', width: '100%'}} />
+
     </div>
   );
 };
